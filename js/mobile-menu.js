aa26e583a99518a2a000f0ef410a82e715a7f795
JEONGSUJI <wtw9611@gmail.com>
2019-09-27 16:39:09 +0900

    flex를 활용한 web cafe 반응형 예제 footer까지 모두 작업 완료

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,9 +1,12 @@
 var viewport = window.matchMedia('(max-width:999px)');
 var items = $('.menu-item');
+var board_list = $('.board-list > li');
 
 $(window).resize(function(){
   location.reload();
 });
+
+board_list.attr('class', 'icon-star');
 
 if(viewport.matches){
   var nav = $('.navigation');
